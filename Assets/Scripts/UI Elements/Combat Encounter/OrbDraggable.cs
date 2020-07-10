173154d9d408090a0ac2a99a333d3ab617656a99
Nizar Kury <nkury@asu.edu>
2020-07-09 23:54:49 -0700

    Adding Combat Encounter UI helpers

---
+++ Assets/Scripts/UI Elements/Combat Encounter/OrbDraggable.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OrbDraggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    [SerializeField]
+    private Canvas _canvas;
+
+    [SerializeField]
+    private RectTransform _rectTransform;
+
+    public RectTransform RectTransform
+    {
+        get
+        {
+            return _rectTransform;
+        }
+    }
+
+    [SerializeField]
+    private CanvasGroup _canvasGroup;
+
+    [SerializeField]
+    private float _alphaFadeValue;
+
+    private OrbType _orbType;
+
+    public OrbType OrbType
+    {
+        get
+        {
+            return _orbType;
+        }
+    }
+
+    private Vector2 _originalAnchoredPosition;
+
+    private Context _context;
+
+    private OrbSlot _inOrbSlot; // for determining if orb is in a spell slot
+    
+    private void Awake()
+    {
+        _originalAnchoredPosition = _rectTransform.anchoredPosition;
+        //_canvas = _context.UIManager.GetActiveCanvas();
+    }
+
+    public void Setup( Context context, OrbType orbType )
+    {
+        _context = context;
+        _orbType = orbType;
+    }
+
+    public void OnBeginDrag( PointerEventData eventData )
+    {
+        Debug.Log( "OnBeginDrag" );
+
+        if( _inOrbSlot )
+        {
+            _inOrbSlot.RemovedOrb();
+            _inOrbSlot = null;
+        }
+
+        _canvasGroup.alpha = _alphaFadeValue;
+        _canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag( PointerEventData eventData )
+    {
+        Debug.Log( "OnDrag" );
+        _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+    }
+
+    public void OnEndDrag( PointerEventData eventData )
+    {
+        Debug.Log( "OnEndDrag" );
+
+        if( _inOrbSlot == null )
+        {
+            _rectTransform.anchoredPosition = _originalAnchoredPosition;
+        }
+
+        _canvasGroup.alpha = 1;
+        _canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown( PointerEventData eventData )
+    {
+        Debug.Log( "OnPointerDown" );
+    }
+
+    public void OnDrop( PointerEventData eventData ) 
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SetOrbSlot(OrbSlot orbSlot)
+    {
+        _inOrbSlot = orbSlot;
+    }
+}
---
+++ Assets/Scripts/UI Elements/Combat Encounter/OrbSlot.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OrbSlot : MonoBehaviour, IDropHandler
+{
+    [SerializeField]
+    private RectTransform _rectTransform;
+
+    private OrbType _orbType;
+
+    private IContext _context;
+
+    private bool _hasOrb;
+
+    private SpellView _spellView;
+
+    public bool HasOrb
+    {
+        get
+        {
+            return _hasOrb;
+        }
+    }
+
+    public void OnDrop( PointerEventData eventData )
+    {
+        if( eventData.pointerDrag != null )
+        {
+            OrbDraggable orbDraggable = eventData.pointerDrag.GetComponent<OrbDraggable>();
+            if( orbDraggable.OrbType == _orbType )
+            {
+                orbDraggable.SetOrbSlot( this );
+                Vector2 pos = _rectTransform.position;
+                //pos.y -= _rectTransform.rect.width / 4;
+                //pos.x += _rectTransform.rect.height / 4;
+                orbDraggable.RectTransform.position = pos;
+                _hasOrb = true;
+            }
+        }
+    }
+
+    public void Setup( IContext context, OrbType orbType, SpellView spellView )
+    {
+        _orbType = orbType;
+        _context = context;
+        _spellView = spellView;
+    }
+
+    public void RemovedOrb()
+    {
+        _hasOrb = false;
+    }
+}
