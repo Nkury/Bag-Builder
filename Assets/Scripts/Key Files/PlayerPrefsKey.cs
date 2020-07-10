e16dc30ccd9502986e66e50bfb9beea65b3ee0b8
Nizar Kury <nkury@asu.edu>
2020-07-09 23:58:05 -0700

    Adding PlayerPrefsKey for data persistence

---
+++ Assets/Scripts/Key Files/PlayerPrefsKey.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerPrefsKey
+{
+    public static readonly string RUN_COUNT = "run_count";
+}
