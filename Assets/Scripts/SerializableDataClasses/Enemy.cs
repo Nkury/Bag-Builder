c2760247f6c3ef2962c516bcf74a40d9e86b57b1
Nizar Kury <nkury@asu.edu>
2020-07-09 23:53:11 -0700

    Serializable data classes

---
+++ Assets/Scripts/SerializableDataClasses/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum EnemyType
+{
+    IMP,
+    GOBLIN,
+    BAT,
+    WORM_TERROR,
+    WISPS,
+    SLIME,
+    HANDS,
+    PIN_CUSHION,
+    ABOMINATION
+}
+
+[Serializable]
+public class Enemy
+{
+    public EnemyType EnemyType;
+    public int Health;
+    public int Draw;
+    public string Name;
+    public string BagName;
+    public List<string> SpellNames;
+    public int BaseXP;
+    public int BaseGold;
+
+    public virtual void DrawOrb()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SerializableDataClasses/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class LevelUp
+{
+    public int ExperienceThreshold;
+    public int Level;
+    public string Title;
+}
+
+[Serializable]
+public class Player
+{
+    public int Health;
+    public int HealthMax;
+    public int Mana;
+    public int ManaMax;
+    public int CurrentSkull;
+    public int SkullCap;
+    public float OverloadMultiplier;
+    public float OverloadLimit;
+    public int Experience;
+    public int Level;
+    public int DamageModifier;
+    public int DefenseModifier;
+    public int AccessorySlots;
+    public List<string> Spells = new List<string>(); // names of the spells the player has
+    public string Title;
+    public List<LevelUp> LevelUpChart = new List<LevelUp>();
+}
